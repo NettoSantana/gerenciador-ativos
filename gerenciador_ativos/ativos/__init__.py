f15acf4debdc7fe135938f16e5da6dbe40155774
Seu Nome <seuemail@exemplo.com>
2025-12-01 21:45:11 -0300

    ﻿fix: ajusta nome do blueprint de ativos para 'ativo_bp' - corrige registro no __init__.py - prepara estrutura para painel e futuras rotas

--- gerenciador_ativos/ativos/__init__.py
+++ gerenciador_ativos/ativos/__init__.py
@@ -4,7 +4,7 @@ ativos_bp = Blueprint(
     "ativos",
     __name__,
     url_prefix="/ativos",
-    template_folder="../../templates/ativos"
+    template_folder="../templates/ativos"
 )
 
 from gerenciador_ativos.ativos import routes  # noqa
