dacda9a871320411c3ac830660376cc6124a4f83
Seu Nome <seuemail@exemplo.com>
2025-12-29 16:57:15 -0300

    ﻿fix: corrige caminho do template do dashboard TV

--- gerenciador_ativos/dashboards/geral.py
+++ gerenciador_ativos/dashboards/geral.py
@@ -2,10 +2,9 @@ from flask import Blueprint, render_template
 
 dashboard_geral_bp = Blueprint(
     "dashboard_geral",
-    __name__,
-    template_folder="templates"
+    __name__
 )
 
 @dashboard_geral_bp.route("/dashboard-geral")
 def dashboard_geral():
-    return render_template("painel_tv.html")
+    return render_template("dashboards/painel_tv.html")
